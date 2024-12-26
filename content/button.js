f53f6071647dd6101edc813b8dc65387b89453a4
Johnny <19619630+jcnmsg@users.noreply.github.com>
2024-12-26 20:48:38 +0000

    Remove stuff that was not needed

--- content/button.js
+++ content/button.js
@@ -1,8 +1,42 @@
+function Button() {
+    return DIV(
+        DIV(
+            [DIV(
+                BUTTON(
+                    SPAN('Download', 'Button__labelItem'),
+                    'https://annas-archive.org/search?q=',
+                    'Button Button--buy Button--medium Button--block dl-button',
+                    'background: black; color: white; border: none; outline: none; box-shadow: none; user-select: none;'
+                ),
+                'Button__container Button__container--block',
+            ), 
+            DIV(
+                BUTTON(
+                    SPAN('💀', 'Button__labelItem'),
+                    'https://libgen.li/index.php?req=',
+                    'Button Button--buy Button--medium Button--rounded dl-button',
+                    'background: black; border: none; color: white; border-left: 1px solid white; outline: none; box-shadow: none; user-select: none;'
+                ),
+                'Button__container',
+            )],
+            'ButtonGroup ButtonGroup--block',
+        ),
+        'BookActions__button'
+    );
+}
+
+function openLink(url) {
+    const titleAndAuthor = `${document.querySelector('.Text__title1').textContent} ${document.querySelector('.ContributorLink__name').textContent}`;
+    const finalUrl = `${url}${encodeURIComponent(titleAndAuthor)}`;
+
+    return window.open(finalUrl, '_blank');
+}
+
 function SPAN(textContent, className) {
     const span = document.createElement('span');
     span.textContent = textContent;
     span.className = className;
-    
+
     return span;
 }
 
@@ -12,10 +46,10 @@ function BUTTON(child, link, className, cssText) {
     btn.className = className;
     btn.style.cssText = cssText;
 
-    btn.appendChild(child); 
-    
+    btn.appendChild(child);
+
     btn.addEventListener('click', () => openLink(link));
-    
+
     return btn;
 }
 
@@ -32,46 +66,4 @@ function DIV(children, className, cssText) {
     }
 
     return div;
-}
-
-function Button() {
-    const annasArchiveUrl = 'https://annas-archive.org/search?q=';
-    const libgenUrl = 'https://libgen.li/index.php?req=';
-
-    const downloadButtonContainer = DIV(
-        BUTTON(
-            SPAN('Download', 'Button__labelItem'), 
-            annasArchiveUrl,
-            'Button Button--buy Button--medium Button--block dl-button', 
-            'background: black; color: white; border: none; outline: none; box-shadow: none; user-select: none;'
-        ),
-        'Button__container Button__container--block', 
-    );
-
-    const skullButtonContainer = DIV(
-        BUTTON(
-            SPAN('💀', 'Button__labelItem'), 
-            libgenUrl,
-            'Button Button--buy Button--medium Button--rounded dl-button', 
-            'background: black; border: none; color: white; border-left: 1px solid white; outline: none; box-shadow: none; user-select: none;'
-        ),
-        'Button__container', 
-    );
-
-    const wrapper = DIV(
-        DIV(
-            [downloadButtonContainer, skullButtonContainer],
-            'ButtonGroup ButtonGroup--block', 
-        ),
-        'BookActions__button'
-    );
-
-    return wrapper;
-}
-
-function openLink(url) {
-    const titleAndAuthor = `${document.querySelector('.Text__title1').textContent} ${document.querySelector('.ContributorLink__name').textContent}`;
-    const finalUrl = url + encodeURIComponent(titleAndAuthor);
-
-    return window.open(finalUrl, '_blank');
 }
