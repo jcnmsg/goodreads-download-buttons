16187437b4a7540714185ec638b68aeadc1cd6e9
Johnny <19619630+jcnmsg@users.noreply.github.com>
2024-12-27 00:17:40 +0000

    Cleanup

--- content/main.js
+++ content/main.js
@@ -1,14 +1,10 @@
-const containers = document.querySelectorAll('.BookActions');
+[...document.querySelectorAll('.BookActions')].forEach((container, index) => {
+    const buttons = container.querySelectorAll('.BookActions__button');
 
-if (containers.length > 0) {
-    containers.forEach((container, index) => {
-        const buttons = container.querySelectorAll('.BookActions__button');
+    if (buttons.length > 1) {
+        const lastButton = buttons[buttons.length - 2];
 
-        if (buttons.length > 0) {
-            const lastButton = buttons[buttons.length - 2];
-
-            lastButton.insertAdjacentElement('afterend', Button('Download eBook', '📖', 'https://annas-archive.org/search?q=', 'https://libgen.li/index.php?req='));
-            lastButton.insertAdjacentElement('afterend', Button('Download Audiobook', '🎧', 'https://audiobookbay.lu/?s='));
-        }
-    });
-} 
+        lastButton.insertAdjacentElement('afterend', Button('Download eBook', '📖', 'https://annas-archive.org/search?q=', 'https://libgen.li/index.php?req='));
+        lastButton.insertAdjacentElement('afterend', Button('Download Audiobook', '🎧', 'https://audiobookbay.lu/?s='));
+    }
+});
