0275e7c32adc878bbaf1cc4375fb4e2fe84d7b9d
Johnny <19619630+jcnmsg@users.noreply.github.com>
2025-01-07 20:49:28 +0000

    Update libgen link

--- content/main.js
+++ content/main.js
@@ -4,7 +4,7 @@
     if (buttons.length > 1) {
         const lastButton = buttons[buttons.length - 2];
 
-        lastButton.insertAdjacentElement('afterend', Button('Download eBook', '📖', 'https://annas-archive.org/search?q=', 'https://libgen.is/index.php?req='));
+        lastButton.insertAdjacentElement('afterend', Button('Download eBook', '📖', 'https://annas-archive.org/search?q=', 'https://libgen.li/index.php?req='));
         lastButton.insertAdjacentElement('afterend', Button('Download Audiobook', '🎧', 'https://audiobookbay.lu/?s='));
     }
 });
