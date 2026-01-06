f2c1e3921fe6c01ab0aad44bec2a5d15e4ccc534
Johnny <19619630+jcnmsg@users.noreply.github.com>
2026-01-06 20:34:47 +0000

    Fix syntax error in main.js for LibGen button

--- content/main.js
+++ content/main.js
@@ -5,7 +5,7 @@
         const lastButton = buttons[buttons.length - 2];
 
         lastButton.insertAdjacentElement('afterend', Button('Download on Anna\'s Archive', '🅰️', 'https://annas-archive.li/search?q='));
-        lastButton.insertAdjacentElement('afterend', Button('Download on LibGen', '🚢', 'https://libgen.li/index.php?req=');
+        lastButton.insertAdjacentElement('afterend', Button('Download on LibGen', '🚢', 'https://libgen.li/index.php?req='));
         lastButton.insertAdjacentElement('afterend', Button('Download on ABB', '🎧', 'https://audiobookbay.lu/?s='));
     }
 });
