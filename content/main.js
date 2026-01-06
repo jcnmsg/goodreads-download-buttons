d531019de9844328accc121763d19e1b80e7cdc3
Johnny <19619630+jcnmsg@users.noreply.github.com>
2026-01-06 20:33:45 +0000

    Update Anna's Archive link after .org takedown, separate button links

--- content/main.js
+++ content/main.js
@@ -4,7 +4,8 @@
     if (buttons.length > 1) {
         const lastButton = buttons[buttons.length - 2];
 
-        lastButton.insertAdjacentElement('afterend', Button('Download eBook', '📖', 'https://annas-archive.org/search?q=', 'https://libgen.li/index.php?req='));
-        lastButton.insertAdjacentElement('afterend', Button('Download Audiobook', '🎧', 'https://audiobookbay.lu/?s='));
+        lastButton.insertAdjacentElement('afterend', Button('Download on Anna\'s Archive', '🅰️', 'https://annas-archive.li/search?q='));
+        lastButton.insertAdjacentElement('afterend', Button('Download on LibGen', '🚢', 'https://libgen.li/index.php?req=');
+        lastButton.insertAdjacentElement('afterend', Button('Download on ABB', '🎧', 'https://audiobookbay.lu/?s='));
     }
 });
